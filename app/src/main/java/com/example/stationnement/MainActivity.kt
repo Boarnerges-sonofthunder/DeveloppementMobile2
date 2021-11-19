92414255d43db95f7d49b7c65192f02c39df22ea
Bootchano <paginrence@gmail.com>
2021-11-19 12:18:30 -0500

    ajout d'un alertDialog

--- app/src/main/java/com/example/stationnement/MainActivity.kt
+++ app/src/main/java/com/example/stationnement/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.stationnement
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
@@ -16,17 +14,25 @@ class MainActivity : AppCompatActivity() {
 
         var listview = findViewById<ListView>(R.id.lstStationnement)
         val arraylist = mutableListOf<Statio>()
+        val alertDialogBuilder = AlertDialog.Builder(this)
 
         arraylist.add(Statio("disponible"))
         arraylist.add(Statio("disponible"))
         arraylist.add(Statio("disponible"))
         arraylist.add(Statio("disponible"))
         arraylist.add(Statio("disponible"))
+
+        listview.adapter = MyAdapter(this,R.layout.contenu_list,arraylist)
 
         listview.setOnItemClickListener{ parent:AdapterView<*>, view: View, position:Int, id:Long ->
-            
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            builder.setTitle("Enregistrement")
+            val dialogLayout = inflater.inflate(R.layout.form_alert, null)
+            val editText  = dialogLayout.findViewById<EditText>(R.id.editText)
+            builder.setView(dialogLayout)
+            builder.setPositiveButton("OK") { dialogInterface, i -> Toast.makeText(applicationContext, "EditText est " + editText.text.toString(), Toast.LENGTH_SHORT).show() }
+            builder.show()
         }
-
-
     }
 }
